a5a6076a61488e8aff35111c7782d7c4ef4d66fe
barryfxz <159066418+barryfxz@users.noreply.github.com>
2025-12-24 06:34:02 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,28 +1,28 @@
 import '../styles/globals.css'
-import { WagmiConfig, createClient, configureChains } from 'wagmi'
-import { mainnet, sepolia } from '@wagmi/chains'
+import { WagmiConfig, createConfig, configureChains } from 'wagmi'
+import { mainnet, sepolia } from 'wagmi/chains'
 import { w3mProvider, w3mConnectors } from '@web3modal/ethereum'
 import { EthereumClient } from '@web3modal/ethereum'
 import { Web3Modal } from '@web3modal/react'
 
 const projectId = '962425907914a3e80a7d8e7288b23f62'
 
-const { chains, provider } = configureChains(
+const { chains, publicClient } = configureChains(
   [mainnet, sepolia],
   [w3mProvider({ projectId })]
 )
 
-const wagmiClient = createClient({
+const wagmiConfig = createConfig({
   autoConnect: true,
   connectors: w3mConnectors({ projectId, chains }),
-  provider
+  publicClient
 })
 
-const ethereumClient = new EthereumClient(wagmiClient, chains)
+const ethereumClient = new EthereumClient(wagmiConfig, chains)
 
 export default function App({ Component, pageProps }) {
   return (
-    <WagmiConfig client={wagmiClient}>
+    <WagmiConfig config={wagmiConfig}>
       <Component {...pageProps} />
       <Web3Modal
         projectId={projectId}
