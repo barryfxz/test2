4eb2d5e6ccac463947318b80f64aa3dad57185e5
barryfxz <159066418+barryfxz@users.noreply.github.com>
2025-12-24 15:33:21 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,28 +1,25 @@
-import { WagmiConfig, createClient } from 'wagmi'
-import { mainnet } from 'wagmi/chains'
-import { w3mConnectors, w3mProvider } from '@web3modal/ethereum'
-import { Web3Modal } from '@web3modal/react'
+import '@/styles/globals.css'
+import { createAppKit } from '@reown/appkit'
 
 const projectId = '962425907914a3e80a7d8e7288b23f62'
-const chains = [mainnet]
 
-const wagmiClient = createClient({
-  autoConnect: true,
-  connectors: w3mConnectors({ projectId, version: 2, chains }),
-  provider: w3mProvider({ projectId, chains })
+// Initialize AppKit ONCE
+createAppKit({
+  projectId,
+  chains: [
+    {
+      id: 1,
+      name: 'Ethereum',
+      rpcUrl: 'https://cloudflare-eth.com'
+    }
+  ],
+  appearance: {
+    theme: 'dark',
+    accentColor: '#22c55e',
+    borderRadius: '16px'
+  }
 })
 
 export default function App({ Component, pageProps }) {
-  return (
-    <>
-      <WagmiConfig client={wagmiClient}>
-        <Component {...pageProps} />
-      </WagmiConfig>
-
-      <Web3Modal
-        projectId={projectId}
-        ethereumClient={wagmiClient}
-      />
-    </>
-  )
+  return <Component {...pageProps} />
 }
